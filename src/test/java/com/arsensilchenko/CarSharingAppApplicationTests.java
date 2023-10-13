82fc039acd27fae8d4af3dc260d48303576693a3
silchenko-arsen <silchenkoarsen@gmail.com>
2023-10-13 13:17:12 +0300

    Initial commit

---
+++ src/test/java/com/arsensilchenko/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arsensilchenko.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
